f7728503ccb89b920bd3537533e3a1f8ec7ac809
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-05-23 14:26:45 +0800

    github token api

--- .history/src/pages/Home_20230523141730.jsx
+++ .history/src/pages/Home_20230523141730.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
       <div>
           <h1 className='text-6xl'>Welcome</h1>
-          ${import.meta.env.VITE_GITHUB_TOKEN}
+          {process.env.REACT_APP_GITHUB_TOKEN}
     </div>
   )
 }
